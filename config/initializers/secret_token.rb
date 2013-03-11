2e6bb1567a21da2a4682ec8f8d330a4508c78236
Daniel <raabhim@gmail.com>
2013-03-11 00:12:05 -0700

    upload repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spendy::Application.config.secret_token = 'c62f6fdfdc7c8b0c7fe9a321f2fbccd67070b3330573c19e5d6026e795ee2d77998e3140126dede8dcd2ea460b06ed54156f225b9f5248750858d149b585e52b'
